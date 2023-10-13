b0e9e485bbf04c1428c4d4fb84f925347fa47ebd
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2023-10-13 23:12:11 +0200

    fix: added translation for slash commands

--- src/commands/slashCommands/play.ts
+++ src/commands/slashCommands/play.ts
@@ -1,5 +1,6 @@
 import { CommandInteraction, SlashCommandBuilder, TextChannel } from 'discord.js';
 import { client } from 'src/Bot';
+import i18n from 'src/config/i18n';
 import { fetchGuild } from 'src/database/queries/guilds/get';
 
 import { playCommand } from '../textCommands';
@@ -13,7 +14,12 @@ export async function execute(interaction: CommandInteraction) {
   const guild = await fetchGuild(interaction.guildId!);
 
   const channel = client.channels.cache.get(interaction.channelId!);
-  if (!(channel instanceof TextChannel)) return interaction.reply('Error: Text channel not found !');
+  if (!(channel instanceof TextChannel))
+    return interaction.reply(
+      `${i18n.t('embedsText.errors.arguments.unknown.title')} ${i18n.t(
+        'embedsText.errors.arguments.textChannelNotFound'
+      )} !`
+    );
 
   const textChannel = channel as TextChannel;
   //@ts-expect-error : voiceChannel is not a property of interaction.member
@@ -21,13 +27,29 @@ export async function execute(interaction: CommandInteraction) {
   //@ts-expect-error : getString is not a property of interaction.options
   const query = interaction.options.getString('query');
 
-  if (!guild) return interaction.reply('Error: Guild not found !');
-  if (!voiceChannel) return interaction.reply('Error: You must be in a voice channel !');
-  if (!textChannel) return interaction.reply('Error: Text channel not found !');
-  if (!query) return interaction.reply('Error: You must provide a query !');
+  if (!guild)
+    return interaction.reply(
+      `${i18n.t('embedsText.errors.arguments.unknown.title')} ${i18n.t('embedsText.errors.arguments.guildNotFound')} !`
+    );
+  if (!voiceChannel)
+    return interaction.reply(
+      `${i18n.t('embedsText.errors.arguments.unknown.title')} ${i18n.t(
+        'embedsText.errors.arguments.voiceChannelNotFound'
+      )} !`
+    );
+  if (!textChannel)
+    return interaction.reply(
+      `${i18n.t('embedsText.errors.arguments.unknown.title')} ${i18n.t(
+        'embedsText.errors.arguments.textChannelNotFound'
+      )} !`
+    );
+  if (!query)
+    return interaction.reply(
+      `${i18n.t('embedsText.errors.arguments.unknown.title')} ${i18n.t('embedsText.errors.arguments.queryNotFound')} !`
+    );
 
   await playCommand(['play', query], guild.guildId, textChannel, interaction.user, voiceChannel);
-  interaction.reply('Song requested !');
+  interaction.reply(`${i18n.t('global.understood')} !`);
   interaction.deleteReply();
   return;
 }
